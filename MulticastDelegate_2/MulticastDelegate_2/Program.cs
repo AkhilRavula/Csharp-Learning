716123cf5904b53c7052dec896f0183e20871538
AkhilRavula <95293687+AkhilRavula@users.noreply.github.com>
2022-03-19 12:58:51 +0530

    Add files via upload

---
+++ MulticastDelegate_2/MulticastDelegate_2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace MulticastDelegate_2
+{
+    class MulticastDelLearn
+    {
+        public delegate void rect(double width, double height); //single delegate calling both methods
+        public static void Main()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.GetArea(12.33, 13.33);
+            rectangle.GetPerimeter(12, 14);
+            rect rect1 = new rect(rectangle.GetArea);
+            // rect rect2 = rectangle.GetPerimeter;//we can directly bind method to delegate other than sending to constructur
+            rect1 += rectangle.GetPerimeter;
+
+            rect1.Invoke(12, 14);//this will invoke both methods
+            //if the both methods are having return type only method 2 value is stored or returned as it overwrites the 1st one return.
+            //make sure when we use multicast delegates we sud have or use void methods
+        }
+    }
+
+}
---
+++ MulticastDelegate_2/MulticastDelegate_2/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate_2
+{
+    internal class Rectangle
+    {
+        public void GetArea(double width,double length)
+        {
+            Console.WriteLine(width * length);
+        }
+
+        public void GetPerimeter(double width,double heiight)
+        {
+            Console.WriteLine(2 * (width + heiight));
+        }
+    }
+}
